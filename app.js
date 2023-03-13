f9da272ee55041ac2f742811ba55eb21704b56bc
Bastien Nizart <bastien@nizart.me>
2023-03-13 19:34:26 +0100

    :sparkles: add delete command

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import chalk from 'chalk'
 import fs from 'fs';
 
 import { DATA_PATH, DATA } from './lib/data.js'
+import { delete_command } from './lib/commands/delete.js'
 import { reset } from './lib/commands/reset.js'
 import { book } from './lib/commands/book.js'
 import { copy } from './lib/commands/copy.js'
@@ -12,7 +13,7 @@ import { all } from './lib/commands/all.js'
 import { put } from './lib/commands/put.js'
 
 const program = new Command();
-const VERSION = "0.5.7"
+const VERSION = "0.5.8"
 const DESCRIPTION = "MemoDOC is a small cli program for memorizing and outputting specific commands very quickly. No more scrolling for hours in the documentation"
 
 program
@@ -39,6 +40,11 @@ program.command('copy <book> [key]')
     .alias('cp')
     .description('Copy specific command in your clipboard')
     .action(function (book, key) {copy(book, key)});
+
+program.command('delete <book> [key]')
+    .alias('del')
+    .description('Delete specific command')
+    .action(function (book, key) {delete_command(book, key)});
 
  program.command('reset')
     .description('Reset all data stored in this app')
